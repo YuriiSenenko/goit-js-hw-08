c0f56a64e3aab6d7457fd7758c9bea99ede9d738
YuriiSenenko <yurkosenenko@gmail.com>
2022-09-03 18:19:44 +0300

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+player.on('play', onPlay);
+
+player.on('eventName', function (data) {
+  // data is an object containing properties specific to that event
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[type=email]'),
+  message: document.querySelector('textarea'),
+};
+
+populateTextarea();
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', onSubmitButton);
+
+function onTextareaInput(event) {
+  formData[event.target.name] = event.target.value;
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+// функція відправки
+// видаляється автооновлення сторінки
+// очищається форма
+// очищається сховище піся відправки
+function onSubmitButton(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+//Отримую значення з сховища
+//якщо там було значення, обновляю DOM
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseSavedMsg = JSON.parse(savedMessage);
+  if (savedMessage) {
+    refs.email.value = parseSavedMsg.email;
+    refs.message.value = parseSavedMsg.message;
+    return parseSavedMsg;
+  }
+}
